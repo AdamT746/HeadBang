863aec4a8cddad0f12a054646f510e223a1504ac
AdamT746 <60108446+AdamT746@users.noreply.github.com>
2021-03-04 17:14:12 +0000

    Moving background

---
+++ CGP603/Assets/Scripts/TextMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextMove : MonoBehaviour
+{
+
+    public float TSpeedX;
+    public float TSpeedY;
+    float moveX;
+    float moveY;
+
+    void Update()
+    {
+        moveX = (TSpeedX * Time.deltaTime) + moveX;
+        moveY = (TSpeedY * Time.deltaTime) + moveY;
+        if (moveX > 1) moveX = 0;
+        if (moveY > 1) moveY = 0;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(moveX, moveY);
+    }
+}
