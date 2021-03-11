0a9599daed5275861c19b2c35d0ad4d48ddc1061
Dan915 <33253673+Dan915@users.noreply.github.com>
2021-03-11 14:01:02 +0000

    Pause

---
+++ CGP603/Assets/Daniel/pauseScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseScript : MonoBehaviour
+{
+    Animator animator;
+    public AudioSource audioSource; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void Pause()
+    {
+        animator.SetTrigger("Toggle");
+        Time.timeScale = 0;
+        audioSource.Pause();
+    }
+    public void Resume()
+    {
+        animator.SetTrigger("Toggle");
+        StartCoroutine(StartResume());
+    }
+    IEnumerator StartResume()
+    {    
+        float pauseTime = Time.realtimeSinceStartup + 1f;
+        while (Time.realtimeSinceStartup < pauseTime)
+            yield return 0;     
+        Time.timeScale = 1;
+        audioSource.Play();
+    }
+}
