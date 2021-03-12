da40bd0ed834be66f1b1236ee438ff0494ec25e6
AdamT746 <60108446+AdamT746@users.noreply.github.com>
2021-03-12 16:59:15 +0000

    SceneTraversal
    Organised scene folder
    Set up scene build order
    Added scene traversal functionality to start menu

---
+++ CGP603/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    
+	// Start is called before the first frame update
+	public Button StartButton;
+
+	void Start()
+	{
+		Button btn = StartButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("You have clicked the button!");
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
