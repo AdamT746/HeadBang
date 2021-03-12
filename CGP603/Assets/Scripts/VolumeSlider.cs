51f1cf2169409d001bb8fe0d7f8d35b1862d9d38
AdamT746 <60108446+AdamT746@users.noreply.github.com>
2021-03-12 15:39:16 +0000

    Volume Slider

---
+++ CGP603/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSlider : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void Volume (float sliderValue)
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+    }
+}
