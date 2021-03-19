4380e0b28812f0251996f360dde881e9a92e911b
AdamT746 <60108446+AdamT746@users.noreply.github.com>
2021-03-19 17:21:49 +0000

    Fancy Fonts

--- CGP603/Assets/Scripts/MenuManager.cs
+++ CGP603/Assets/Scripts/MenuManager.cs
@@ -8,20 +8,11 @@ public class MenuManager : MonoBehaviour
 {
     
 	// Start is called before the first frame update
-	public Button StartButton;
-	public Button QuitButton;
-	public Button MenuButton;
+
 
 	void Start()
 	{
-		Button Start = StartButton.GetComponent<Button>();
-		Start.onClick.AddListener(StartGame);
-
-		Button Quit = QuitButton.GetComponent<Button>();
-		Quit.onClick.AddListener(QuitGame);
-
-		Button Exit2Menu = MenuButton.GetComponent<Button>();
-		Exit2Menu.onClick.AddListener(ExitToMenu);
+		
 
 	}
 
