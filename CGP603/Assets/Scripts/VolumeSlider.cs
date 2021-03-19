09cd9b2871ee360ca52d737542105751b508b297
AdamT746 <60108446+AdamT746@users.noreply.github.com>
2021-03-19 15:29:30 +0000

    Volume sliders

--- CGP603/Assets/Scripts/VolumeSlider.cs
+++ CGP603/Assets/Scripts/VolumeSlider.cs
@@ -7,8 +7,12 @@ public class VolumeSlider : MonoBehaviour
 {
     public AudioMixer mixer;
 
-    public void Volume (float sliderValue)
+    public void MusicVolume (float sliderValue)
     {
         mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+    }
+    public void SFXVolume (float sliderValue)
+    {
+        mixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
     }
 }
