db33df11f3f2de0c53873a38f80f8f364e8598e2
Beijinho <60109149+Beijinho@users.noreply.github.com>
2021-03-26 15:53:05 +0000

    PlayerHealth Fix

--- CGP603/Assets/Scripts/PlayerHealth.cs
+++ CGP603/Assets/Scripts/PlayerHealth.cs
@@ -6,13 +6,11 @@ public class PlayerHealth : MonoBehaviour
 {
 
     public int Health;
-    public int Damage;
     public int CurHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        Damage = 20;
         Health = 100;
 
         CurHealth = Health;
@@ -21,20 +19,15 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CurHealth = Health;
-
-        if (CurHealth <= 0)
+        if (Health <= 0)
         {
             Debug.Log("Dead");
         }
-
-
     }
 
     void TakeDamage(int damage)
     {
-        CurHealth = CurHealth - damage;
-
+        CurHealth -= damage;
     }
 
     private void OnTriggerEnter(Collider collision)
