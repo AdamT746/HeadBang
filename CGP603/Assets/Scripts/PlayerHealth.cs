e26831b569ef3068749e205469c8ad1818e535d7
AdamT746 <60108446+AdamT746@users.noreply.github.com>
2021-03-31 13:53:39 +0100

    Death state and health UI

--- CGP603/Assets/Scripts/PlayerHealth.cs
+++ CGP603/Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
 
     public int Health;
     public int CurHealth;
+    public Text HealthText;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +22,13 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Health <= 0)
+        if (CurHealth <= 0)
         {
             Debug.Log("Dead");
+            SceneManager.LoadScene("LevelEnd");
         }
+
+        HealthText.text = "Health " + CurHealth;
     }
 
     void TakeDamage(int damage)
